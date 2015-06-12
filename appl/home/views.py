8a472d52c2a77bbf86f270fa7e5db9faf65abf87
hbrls <shuaizhexu@gmail.com>
2015-06-12 18:05:14 +0800

    获取access token http://mp.weixin.qq.com/wiki/11/0e4b294685f817b95cbed85ba5e82b8f.html

--- appl/home/views.py
+++ appl/home/views.py
@@ -1,17 +1,32 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
-from flask.ext.login import login_required
+import logging
+from uuid import uuid4
+from flask import Blueprint, render_template, current_app, jsonify
+
+
+_logger = logging.getLogger(__name__)
 
 
 mod = Blueprint('home', __name__, template_folder='templates')
 
 
 @mod.route('/')
-@login_required
 def index():
+    _logger.debug('index debug')
+    _logger.info('index info')
+    _logger.warn('index warn')
+    _logger.error('index error')
+
     return render_template('home/index.html')
 
 
-@mod.route('/not_ready')
-def not_ready():
-    return render_template('errors/not_ready.html')
+@mod.route('/cgi-bin/token')
+def token():
+    """
+    获取access token
+    http://mp.weixin.qq.com/wiki/11/0e4b294685f817b95cbed85ba5e82b8f.html
+    """
+    return jsonify({
+        'access_token': str(uuid4()),
+        'expires_in': 7200,
+    })
